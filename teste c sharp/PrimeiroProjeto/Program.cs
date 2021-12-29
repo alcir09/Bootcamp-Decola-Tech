384282651d365a7674f4a6e345701b9de0193057
alcir09 <alcir.amorim9@gmail.com>
2021-12-29 15:45:57 -0300

    adição de soma

--- teste c sharp/PrimeiroProjeto/Program.cs
+++ teste c sharp/PrimeiroProjeto/Program.cs
@@ -4,9 +4,13 @@
 
         static void Main(string[] args) {
             
+            var num1 = 2;
+            var num2 = 7;
+
             var a = "Hello";
             var b = " World!";
             Console.WriteLine(a + b);
+            Console.WriteLine(num1 + num2);
         }
     }
 }
