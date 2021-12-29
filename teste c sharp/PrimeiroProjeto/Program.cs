4d3f5a4b01543edb8ff88648acaafe82cdb8fe82
alcir09 <alcir.amorim9@gmail.com>
2021-12-29 15:26:22 -0300

    Primeiro commit programa olá mundo

---
+++ teste c sharp/PrimeiroProjeto/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Primeiro {
+
+    class Program {
+
+        static void Main(string[] args) {
+            
+            var a = "Hello";
+            var b = " World!";
+            Console.WriteLine(a + b);
+        }
+    }
+}
